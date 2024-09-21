f472513f761fb49bfaff0938a917213c6f8810cc
a.bordiug <a.bordiug@gmail.com>
2024-09-21 16:02:15 +0300

    done

--- task1.js
+++ task1.js
@@ -5,3 +5,18 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+const userList = document.getElementsByClassName('usersList')[0];
+
+async function fetchUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.textContent = user.name;
+    userList.appendChild(li);
+  });
+}
+
+fetchUsers();
